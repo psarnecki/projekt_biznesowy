abbeba86c886ed0736904a5963cbc4906d3cb4d4
Admin <Admin@DESKTOP-LC407O9>
2026-04-14 21:45:19 +0200

    entities

---
+++ Tripder/src/Tripder.Domain/AttractionDefinition/Entities/Tag.cs
@@ -0,0 +1,14 @@
+namespace Tripder.Domain.AttractionDefiniton.Entities;
+public class Tag
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; } = string.Empty;
+
+    private Tag() { }
+
+    public Tag(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
